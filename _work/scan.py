1ec2f59679b64102f264556b5ff2fbc1dabba446
Juan Antonio Pinera Garcia <tonypinera.2015@gmail.com>
2024-09-25 11:46:37 +0200

    Initial commit

---
+++ _work/scan.py
@@ -0,0 +1,39 @@
+# examples/Python/Basic/rgbd_redwood.py
+
+import open3d as o3d
+# from open3d import create_rgbd_image_from_color_and_depth
+import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    print("Read Redwood dataset")
+    color_raw = o3d.io.read_image("./depth.jpeg")
+    depth_raw = o3d.io.read_image("./depth.jpeg")
+
+
+    rgbd_image = o3d.geometry.PointCloud.create_from_rgbd_image(
+        color_raw, color_raw)
+    print(rgbd_image)
+
+    plt.subplot(1, 2, 1)
+    plt.title('Redwood grayscale image')
+    plt.imshow(rgbd_image.color)
+    plt.subplot(1, 2, 2)
+    plt.title('Redwood depth image')
+    plt.imshow(rgbd_image.depth)
+    plt.show()
+
+    pcd = o3d.geometry.create_point_cloud_from_rgbd_image(
+        rgbd_image,
+        o3d.camera.PinholeCameraIntrinsic(
+            o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
+    # Flip it, otherwise the pointcloud will be upside down
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    o3d.visualization.draw_geometries([pcd])
+
+
+
+    # print("Read Redwood dataset")
+    # color_raw = o3d.io.read_image("../../TestData/RGBD/color/00000.jpg")
+    # depth_raw = o3d.io.read_image("../../TestData/RGBD/depth/00000.png")
+    # rgbd_image = o3d.geometry.create_rgbd_image_from_color_and_depth(
+    #     color_raw, depth_raw)
+    # print(rgbd_image)
